eed12e6e4bea66a1e75fb6917a036f4bcfec5935
EzilLize <115159321+EzilLize@users.noreply.github.com>
2022-11-11 14:39:18 +0300

    Add files via upload

---
+++ lesson 15/assets/js/main.js
@@ -0,0 +1,59 @@
+//elem.classList.add('www');
+//console.log(elem);
+
+//elem.classList.remove('www');
+//console.log(elem);
+
+//let contains = elem.classList.contains('www');
+//console.log(contains)
+
+//elem.classList.toggle('www');
+//console.log(elem);
+
+//let lenghtElem = elem.classList.lenght;
+//console.log(lenghtElem);
+
+//elem.style.cssText = 'color: red; font-size:30px; border: 3px black;'
+
+//elem.addEventListener('click', func);
+//function func() {
+//   console.log(elem.tagName);
+//};
+
+//elem.addEventListener('click', func);
+//function func() {
+//   console.log(elem.tagName.toLowerCase());
+//}
+
+//let ol = document.getElementById('man');
+//let li = document.createElement('li');
+//li.innerHTML = 'пункт';
+//ol.appendChild(li);
+
+//let arr = ['To', 'Be', 'God']
+//for (let i=0; i < arr; i++) {
+//    let li = document.createElement('li');
+//    li.innerHTML= arr[i];
+//    ul.appendChild(li);
+//}
+
+//let = document.getElementById('elem');
+//elem.insertAdjacentHTML('beforebegin','<span>!!!</span>');
+
+//let = document.getElementById('elem');
+//elem.insertAdjacentHTML('afterEnd','<span>!!!</span>')
+
+//let = document.getElementById('elem');
+//elem.insertAdjacentHTML('afterBegin','<span>!!!</span>')
+
+//let = document.getElementById('elem');
+//elem.insertAdjacentHTML('BeforeEnd','<span>!!!</span>')
+
+//elem.firstElementChild.style.color = 'red';
+
+//elem.lastElementChild.style.color = 'red';
+//for (let i=0; i < elem.child.length; i++) {
+//    elem.child[i].innerHTML +='!'
+//};
+
+//elem.previousElementSibling.innerHTML +='!'; 
